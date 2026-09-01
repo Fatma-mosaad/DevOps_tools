b51768b31c18897085bf070f66e31d601dd0cd68
Fatma-mosaad <fatima.mosasd7@gmail.com>
2026-09-01 14:29:17 +0300

    Add database initialization

---
+++ jinkins/user-app/init.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL
+);
+
+INSERT INTO users (name, email)
+VALUES
+    ('Fatma', 'fatma@example.com'),
+    ('Ahmed', 'ahmed@example.com')
+ON CONFLICT (email) DO NOTHING;
