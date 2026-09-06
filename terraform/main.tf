5963acbeeeefe330f1d1feaf17532d2dc9bcfee7
Fatma-mosaad <142949799+Fatma-mosaad@users.noreply.github.com>
2026-09-06 15:09:56 +0300

    Refactor SSH cidr_blocks to use variable directly

--- terraform/main.tf
+++ terraform/main.tf
@@ -44,7 +44,7 @@ resource "aws_security_group" "users_app" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [var.ssh_cidr]
+    cidr_blocks = var.ssh_cidr
   }
 
   ingress {
