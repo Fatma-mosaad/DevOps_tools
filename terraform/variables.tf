3732eeff28e2f4d15b2f99994f17c32f6e06f0ca
Fatma-mosaad <142949799+Fatma-mosaad@users.noreply.github.com>
2026-09-12 15:20:38 +0300

    Update descriptions and defaults in variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,25 +1,28 @@
 variable "aws_region" {
   type        = string
   description = "AWS region"
-  default     = "eu-central-1"
+
+  default = "eu-central-1"
 }
 
 variable "ami_id" {
   type        = string
-  description = "Ubuntu AMI ID"
+  description = "Linux AMI ID"
 }
 
 variable "instance_type" {
   type        = string
-  default     = "t3.micro"
+  description = "EC2 instance type"
+
+  default = "t3.micro"
 }
 
 variable "key_name" {
   type        = string
-  description = "AWS EC2 key pair name"
+  description = "Existing EC2 key pair name"
 }
 
 variable "ssh_cidr" {
   type        = list(string)
-  description = "CIDR allowed to access SSH"
+  description = "CIDR ranges allowed to access SSH"
 }
