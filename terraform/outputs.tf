879bed2ec0b3ac72f6b701174c19c1041ce368a5
Fatma-mosaad <142949799+Fatma-mosaad@users.noreply.github.com>
2026-09-12 15:19:54 +0300

    Add output for EC2 instance ID
    Added output for EC2 instance ID in outputs.tf.

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,17 @@
+output "instance_id" {
+  description = "Created EC2 instance ID"
+
+  value = aws_instance.users_app.id
+}
+
 output "instance_public_ip" {
+  description = "Public IP of the EC2 instance"
+
   value = aws_instance.users_app.public_ip
 }
 
 output "instance_public_dns" {
+  description = "Public DNS of the EC2 instance"
+
   value = aws_instance.users_app.public_dns
 }
