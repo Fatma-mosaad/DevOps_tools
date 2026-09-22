7b02581715a08679793f9dbb7e7382e62140cf25
Fatma-mosaad <142949799+Fatma-mosaad@users.noreply.github.com>
2026-09-22 13:21:39 +0300

    Update app.py

--- jinkins/user-app/app.py
+++ jinkins/user-app/app.py
@@ -15,12 +15,12 @@ def get_db_connection():
     )
 
 
-# @app.route("/health")
-# def health():
-#     return jsonify({"status": "ok"})
 @app.route("/health")
 def health():
-    return jsonify({"status": "error"}), 500
+    return jsonify({"status": "ok"})
+# @app.route("/health")
+# def health():
+#     return jsonify({"status": "error"}), 500
     
 
 
